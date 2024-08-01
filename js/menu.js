14e7098fbeb1f7ead3ec3b16f38f5ef767bb0cdd
Alex.dv <alexpereirasilv4@gmail.com>
2024-08-01 13:16:49 -0300

    alterando paleta de cores e organizando melhor a página inicial

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+
+
+function menuColor() {
+
+    let menuCassio = document.getElementById('menuzinho');
+
+    if (menuCassio.style.color == "black")
+        {
+        menuCassio.style.color == "white";
+        }
+}
+
+
+
+
